412913184a1368e5c0e2f38f93d1bda85c16e990
retya <aryh.mothieu@gmail.com>
2022-06-21 20:52:28 +0200

    Exercice Condition

--- src/Components/ShoppingList.js
+++ src/Components/ShoppingList.js
@@ -1,12 +1,15 @@
 // eslint-disable-next-line to the line before.
 import React  from 'react';
 import {plantList} from "../datas/plantList";
+import '../styles/ShoppingList.css'
 
 function ShoppingList(){
-    const cat = []
-    plantList.forEach((plant)=>
-    cat.push(plant.category))
-    console.log(cat)
+
+   const cat = plantList.reduce(
+       (acc, plant) =>
+           acc.includes(plant.category) ? acc : acc.concat(plant.category),
+       []
+   )
 
     return (
         <div>
@@ -15,9 +18,9 @@ function ShoppingList(){
                     <li key={data}>{data}</li>
                     ))}
             </ul>
-            <ul>
+            <ul className='lmj-plant-list'>
                 {plantList.map((plant) => (
-                    <li key={plant.id}>{ plant.name }</li>
+                    <li key={plant.id} className='lmj-plant-item'>{ plant.name }{plant.isSpecialOffer &&<div className='lmj-sales'>Soldes</div>}</li>
                 ))}
             </ul>
         </div>
