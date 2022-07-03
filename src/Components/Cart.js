7a42e55baad6a547e7d61f557c454e72e832d362
retya <aryh.mothieu@gmail.com>
2022-07-03 15:51:29 +0200

    Exercice useState

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,20 +1,46 @@
-
-import React  from 'react';
+import React from "react";
+import { useState } from 'react'
 import '../styles/Cart.css'
 
-function Cart() {
-    let lierre = 10
-    let monstera = 8
-    let fleur = 15
-    return (
+function Cart({ cart, updateCart }) {
+    const [isOpen, setIsOpen] = useState(true)
+    const total = cart.reduce(
+        (acc, plantType) => acc + plantType.amount * plantType.price,
+        0
+    )
+    return isOpen ? (
         <div className='lmj-cart'>
-            <h2>Panier</h2>
-            <ul>
-                <li>Monstera : { monstera }€</li>
-                <li>Lierre : { lierre }€</li>
-                <li>Bouquet de Fleur : { fleur }€</li>
-            </ul>
-            Total : { monstera+lierre+fleur }€
+            <button
+                className='lmj-cart-toggle-button'
+                onClick={() => setIsOpen(false)}
+            >
+                Fermer
+            </button>
+            {cart.length > 0 ? (
+                <div>
+                    <h2>Panier</h2>
+                    <ul>
+                        {cart.map(({ name, price, amount }, index) => (
+                            <div key={`${name}-${index}`}>
+                                {name} {price}€ x {amount}
+                            </div>
+                        ))}
+                    </ul>
+                    <h3>Total : {total}€</h3>
+                    <button onClick={() => updateCart([])}>Vider le panier</button>
+                </div>
+            ) : (
+                <div>Votre panier est vide</div>
+            )}
+        </div>
+    ) : (
+        <div className='lmj-cart-closed'>
+            <button
+                className='lmj-cart-toggle-button'
+                onClick={() => setIsOpen(true)}
+            >
+                Ouvrir le Panier
+            </button>
         </div>
     )
 }
--- src/Components/ShoppingList.js
+++ src/Components/ShoppingList.js
@@ -3,27 +3,49 @@ import React  from 'react';
 import {plantList} from "../datas/plantList";
 import '../styles/ShoppingList.css'
 import PlantItem from './PlantItem'
+import Categories from "./Categories";
+import {useState} from "react";
 
 
 
-function ShoppingList(){
+function ShoppingList({cart,updateCart}){
+    const [activeCategory, setActiveCategory] = useState('')
+    const categories = plantList.reduce(
+        (acc, plant) =>
+            acc.includes(plant.category) ? acc : acc.concat(plant.category),
+        []
+    )
+    function addToCart(name, price) {
+        const currentPlantSaved = cart.find((plant) => plant.name === name)
+        if (currentPlantSaved) {
+            const cartFilteredCurrentPlant = cart.filter(
+                (plant) => plant.name !== name
+            )
+            updateCart([
+                ...cartFilteredCurrentPlant,
+                { name, price, amount: currentPlantSaved.amount + 1 }
+            ])
+        } else {
+            updateCart([...cart, { name, price, amount: 1 }])
+        }
+    }
 
-   const cat = plantList.reduce(
-       (acc, plant) =>
-           acc.includes(plant.category) ? acc : acc.concat(plant.category),
-       []
-   )
     return (
-        <div>
-            <ul>
-                {cat.map((data)=>(
-                    <li key={data}>{data}</li>
-                    ))}
-            </ul>
+        <div className='lmj-shopping-list'>
+            <Categories
+                categories={categories}
+                setActiveCategory={setActiveCategory}
+                activeCategory={activeCategory}
+            />
             <ul className='lmj-plant-list'>
                 {plantList.map((plant) => (
-                    <PlantItem id={plant.id} name={plant.name} cover={plant.cover} light={plant.light} water={plant.water} />
-                ))}
+                    plant.category === activeCategory ?(
+                    <div key={plant.id}>
+                        <PlantItem id={plant.id} name={plant.name} cover={plant.cover} light={plant.light} water={plant.water} />
+                        <button onClick={() => addToCart(plant.name, plant.price)}>Ajouter</button>
+                    </div>):null
+                    ))}
+
             </ul>
         </div>
     )
